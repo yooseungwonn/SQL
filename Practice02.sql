db373991f5b20cc86c3b6b5edda1fcea58547f0c
yooseungwonn <163090539+yooseungwonn@users.noreply.github.com>
2024-04-30 18:09:58 +0900

    Update Practice02.sql
    24. 4. 30

--- Practice02.sql
+++ Practice02.sql
@@ -1,15 +1,14 @@
 -- 문제 1
-SELECT count(*)-1 haveMngCnt 
+SELECT count(manager_id) haveMngCnt 
 FROM employees;
 
 -- 문제 2
-SELECT MAX(salary)-MIN(salary) 차액
+SELECT MAX(salary) 최고임금, MIN(salary) 최저임금, MAX(salary)-MIN(salary) "최고임금 - 최저임금"
 FROM employees;
 
 -- 문제 3
-SELECT TO_CHAR(MAX(hire_date), 'YYYY"년"MM"월"DD"일"') 신입사원등록
+SELECT TO_CHAR(MAX(hire_date), 'YYYY"년" MM"월" DD"일"') 신입사원등록
 FROM employees;
-
 
 -- 문제 4
 SELECT department_id, AVG(salary),
@@ -19,7 +18,7 @@ GROUP by department_id
 ORDER BY department_id DESC;
 
 -- 문제 5
-SELECT job_id, ROUND(AVG(salary), 2),
+SELECT job_id, ROUND(AVG(salary)),
 MAX(salary), MIN(salary)
 FROM employees 
 GROUP by job_id
@@ -32,28 +31,30 @@ FROM employees;
 
 
 -- 문제 7
-SELECT department_id,  ROUND(AVG(salary), 2),
+SELECT department_id, AVG(salary),
 MIN(salary), AVG(salary)-MIN(salary)
 FROM employees
 GROUP by department_id
-ORDER BY ROUND(AVG(salary)-MIN(salary), 2) DESC;
+HAVING AVG(salary)-MIN(salary) < 2000 -- 집계 함수 이후의 조건 점검은 HAVING 절에서 해야 한다
+ORDER BY AVG(salary)-MIN(salary) DESC;
 
 -- 문제 8
-SELECT job_id, MIN(salary),
-MAX(salary), MAX(salary)- MIN(salary)
+SELECT job_id, MAX(salary),
+MIN(salary), MAX(salary)- MIN(salary) diff
 FROM employees
 GROUP by job_id
-ORDER BY MAX(salary)- MIN(salary) DESC;
+ORDER BY diff DESC;
 
 -- 문제 9
-SELECT manager_id, ROUND(AVG(salary), 1),
-MAX(salary), MIN(salary), hire_date
-FROM employees group by manager_id, hire_date
-WHERE (AVG(salary) >= '5000') AND
-(hire_date >= '15/01/01');
+SELECT manager_id, ROUND(AVG(salary)),
+MIN(salary), MAX(salary) FROM employees
+WHERE hire_date >= '15/01/01' -- 집계 함수 실행 이전의 조건 처리
+GROUP BY manager_id
+HAVING AVG(salary) >= 5000  -- 집계 함수 실행 이후의 조건 처리
+ORDER BY AVG(salary) DESC;
 
 -- 문제 10
-SELECT first_name, hire_date,
+SELECT first_name, hire_date, employee_id, salary,
 CASE WHEN hire_date <= '12/12/31' THEN '창립멤버'
      WHEN hire_date <= '13/12/31' THEN '13년입사'
      WHEN hire_date <= '14/12/31' THEN '14년입사'
